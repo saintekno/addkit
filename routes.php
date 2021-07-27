91ac3811864daea807641bb929bac7e0f7a63e11
buddywinangun <buddywinangun@gmail.com>
2021-07-27 19:20:28 +0700

    fixed license

--- routes.php
+++ routes.php
@@ -1,18 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-/**
- * SainSuite
- *
- * Engine Management System
- *
- * @package     SainSuite
- * @copyright   Copyright (c) 2019-2020 Buddy Winangun, Eracik.
- * @copyright   Copyright (c) 2020-2021 SainTekno, SainSuite.
- * @link        https://github.com/saintekno/sainsuite
- * @filesource
- */
-
 global $Routes;
 
 $Routes->group(['prefix' => '/addkit'], function () use ( $Routes ) {
--- views/addkit/read.php
+++ views/addkit/read.php
@@ -1,16 +1,5 @@
 <?php
-
-/**
- * SainSuite
- *
- * Engine Management System
- *
- * @package     SainSuite
- * @copyright   Copyright (c) 2019-2020 Buddy Winangun, Eracik.
- * @copyright   Copyright (c) 2020 SainTekno, SainSuite.
- * @link        https://github.com/saintekno/sainsuite
- * @filesource
- */
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**========================================================================
  *                           Toolbar
